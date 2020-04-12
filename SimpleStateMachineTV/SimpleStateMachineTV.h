dfd2346a84a4702455eba66258b8c4d889eeb3fc
Daniel Sanchez <daniel@nurvv.com>
2020-04-12 21:54:15 +0100

    Adding tvOS target.

---
+++ SimpleStateMachineTV/SimpleStateMachineTV.h
@@ -0,0 +1,18 @@
+//
+//  SimpleStateMachineTV.h
+//  SimpleStateMachineTV
+//
+//  Created by Daniel Sanchez on 12/04/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SimpleStateMachineTV.
+FOUNDATION_EXPORT double SimpleStateMachineTVVersionNumber;
+
+//! Project version string for SimpleStateMachineTV.
+FOUNDATION_EXPORT const unsigned char SimpleStateMachineTVVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleStateMachineTV/PublicHeader.h>
+
+
