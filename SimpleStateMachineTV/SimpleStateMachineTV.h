b59898955e43b3b9122d9ca013d608ecbe2390a9
Daniel Sanchez <daniel@nurvv.com>
2020-04-12 22:41:37 +0100

    Updating public header

--- SimpleStateMachineTV/SimpleStateMachineTV.h
+++ SimpleStateMachineTV/SimpleStateMachineTV.h
@@ -15,4 +15,15 @@ FOUNDATION_EXPORT const unsigned char SimpleStateMachineTVVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <SimpleStateMachineTV/PublicHeader.h>
 
-
+#import "SMAction.h"
+#import "SMActionProtocol.h"
+#import "SMCommon.h"
+#import "SMCompositeAction.h"
+#import "SMDecision.h"
+#import "SMMonitorNSLog.h"
+#import "SMNode.h"
+#import "SMState.h"
+#import "SMStateMachine.h"
+#import "SMStateMachineAsync.h"
+#import "SMStateMachineExecuteContext.h"
+#import "SMTransition.h"
